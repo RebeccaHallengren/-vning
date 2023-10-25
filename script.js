ec0e6ccc7d1b7d80445f99437b2c6da70cec675b
Rebecca <rebecca_hallengren@hotmail.com>
2023-10-25 11:24:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//let mittnamn="Rebecca";
+//console.log(mittnamn);
+
+//Alert är en pop up ruta
+//let favoritdryck = prompt ("Vilken är din favoritdryck?");
+//console.log("Din favoritdryck är" + favoritdryck);
+//alert (favoritdryck);
+
+//när användaren skriver in ett värde tolkar alltid systemet det som en sträng, 
+//let tal = prompt ("Skriv en siffra:");
+//let siffra = Number (tal);
+//let tal2 = prompt ("Skriv en till:");
+//let andrasiffra = Number (tal2);
+
+//function addtwonr(siffra, andrasiffra) {
+  //  return siffra + andrasiffra;
+//}
+
+//let addedValues = addtwonr(siffra, andrasiffra);
+//alert("svaret är:" + addedValues);
+
+
+//promt är när man ber om ett värde från användaren. const är en variabel.
+//const färg = prompt ("Vilken är din favoritfärg?");
+//skapar en funktion anropar HTML knapp och väljer consol log och ansropar det användaren lagt in.
+//function knapp() {
+//    console.log (färg);
+//}
+
+
+
+// 4.Använd prompt och fråga efter en favorit färg och spara 
+//värdet i en global variabel. Skapa även en knapp med ett 
+//onClick-event som kallar på en funktion där färgen skrivs ut 
+//i konsolen med hjälp av funktionen console.log(). 5.Skriv en funktion som tar in ett för- och efternamn och skriver ut dem i en alert() med mellanrum emellan namnen. Kalla sedan på funktionen och skicka in de efterfrågade värdena direkt när sidan läses in (direkt i Javascript-filen).
